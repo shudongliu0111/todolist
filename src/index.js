699df652aa1f4ae5457fcfddda0e164a34e73c45
unknown <970654226@qq.com>
2018-07-22 09:57:23 +0800

    create todolist

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css'; //默认样式可以不用
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();  //暂时不用 import也注释了
