6b01c50263a13cd2fd44362fe81396e385e12018
unknown <970654226@qq.com>
2018-07-22 16:11:51 +0800

    add and remove

---
+++ src/TodoList.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class TodoList extends Component {
+  /*
+    构造函数 初始化就有
+    state 可以储存数据
+  */
+  constructor(props){
+    super(props);
+    this.state = {
+      list:[
+        'render只能返回一个jsx 也就是只有一个div不能返回两个',
+        '要将两个都放在一个div中aaa',
+        'handleBtnClick方法中的this 是指向这个方法而不是这个组件',
+        '需要bind（this）来绑定this 注意一下',
+        '...是展开运算符'
+      ],
+      inputValue: ''
+    }
+  }
+  handleBtnClick(){
+    // this.state.list.push('这是无法改变state的')；
+    if(this.state.inputValue ===''){
+      alert("为空？？");
+    }
+    this.setState({
+      list:[...this.state.list,this.state.inputValue],
+      inputValue:''
+    })
+    
+  }
+  handleInputChange(e){
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleItemClick(index){
+    // console.log(index);  创建一个副本来操作
+    const list = [...this.state.list]
+    list.splice(index, 1);   
+    this.setState({
+      // list:list  如果键和值是相同的 可以直接写
+      list
+    })
+  }
+  render() {
+    return (
+      <div className="TodoList">
+          <div>
+            <input value ={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
+            <button onClick={this.handleBtnClick.bind(this)}>add</button>
+          </div>
+          <div>
+            <ul>
+              {
+                this.state.list.map((item,index) => {
+                  return <li key={index} onClick={this.handleItemClick.bind(this,index)}>{item}</li>
+                })
+              }
+            </ul>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
